510a33b511ad12443af7a7dc051d3975cdab58db
Ryan Duff <ryanduff613@gmail.com>
2020-04-23 12:08:33 -0700

    properly submits api request and displays result of request to user

--- src/main.js
+++ src/main.js
@@ -3,13 +3,27 @@ import './styles.css';
 
 
 $(document).ready(function(){
-  $('#submit').click(function(event){
-    event.preventDefault();
+  $('#submit').click(function(){
   
     const words = parseInt($('#words').val());
-    const paragraph = parseInt($('#paragraphs').val());
+    const paragraphs = parseInt($('#paragraphs').val());
 
-    $('#output').text(words.toString() + paragraph.toString());
+    let request = new XMLHttpRequest();
+    const url = `http://dinoipsum.herokuapp.com/api?format=json&words=${words}&paragraphs=${paragraphs}`;
+
+    request.onreadystatechange = function(){
+      if (this.readyState === 4 && this.status === 200){
+        const response = JSON.parse(this.responseText);
+        showText(response);
+      }
+    }
+
+    request.open('GET', url, true);
+    request.send();
+
+    const showText = function(response) {
+      $('#output').text(`${response}`);
+    }
 
   });
 });
