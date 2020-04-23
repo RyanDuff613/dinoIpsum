02ebc266865b43494a46181c6d9e150971337e49
Ryan Duff <ryanduff613@gmail.com>
2020-04-23 11:46:19 -0700

    display user provided numbers in output div

--- src/main.js
+++ src/main.js
@@ -6,9 +6,10 @@ $(document).ready(function(){
   $('#submit').click(function(event){
     event.preventDefault();
   
+    const words = parseInt($('#words').val());
+    const paragraph = parseInt($('#paragraphs').val());
 
+    $('#output').text(words.toString() + paragraph.toString());
 
-
-    
   });
 });
